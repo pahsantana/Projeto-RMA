1fef86a8438f22f55bde713b6cf7cb81d908b3e6
Paloma Santana <santanagilberto@uol.com.br>
2022-08-30 05:59:59 -0300

    Last adj: Add Pedro's template

---
+++ Assets/RMA-Simulator-main/README.md
@@ -0,0 +1,2 @@
+# RMA-Simulator
+ Simulador RMA
--- Assets/Scripts/CounterDownManager.cs
+++ Assets/Scripts/CounterDownManager.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 public class CounterDownManager : MonoBehaviour
 {
     public GameObject CounterLabel;
-    public AudioSource ReadySound;
-    public AudioSource GoSound;
+    //public AudioSource ReadySound;
+    //public AudioSource GoSound;
     public GameObject Car;
     public GameObject LapTimer;
 
@@ -20,24 +20,24 @@ public class CounterDownManager : MonoBehaviour
     IEnumerator CountDown(){
         yield return new WaitForSeconds(0.03f);
         CounterLabel.GetComponent<Text>().text = "3";
-        ReadySound.Play();
+        //ReadySound.Play();
         CounterLabel.SetActive(true);
         yield return new WaitForSeconds(1);
         CounterLabel.SetActive(false);
         CounterLabel.GetComponent<Text>().text = "2";
-        ReadySound.Play();
+        //ReadySound.Play();
         CounterLabel.SetActive(true);
         yield return new WaitForSeconds(1);
         CounterLabel.SetActive(false);
         CounterLabel.GetComponent<Text>().text = "1";
-        ReadySound.Play();
+        //ReadySound.Play();
         CounterLabel.SetActive(true);
         yield return new WaitForSeconds(1);
         CounterLabel.SetActive(false);
-        GoSound.Play();
+        //GoSound.Play();
 
         //Car.GetComponent<CarController>().enabled = true;
-        //LapTimer.GetComponent<LapTimerDown>().enabled = true;
+        LapTimer.GetComponent<LapTimerDown>().enabled = true;
         
     }
 }
